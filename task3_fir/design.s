ac65427380adae7555d2a3c0811680ae95127b7e
Harsha Pasupuleti <harshapasupuleti859@gmail.com>
2026-04-28 13:46:28 +0530

    Add FIR Filter design documentation in design.s
    Designed a 4-tap FIR Low Pass Filter with specified coefficients and simulation results.

---
+++ task3_fir/design.s
@@ -0,0 +1,30 @@
+# CODTECH VLSI Internship - Task 3
+
+## FIR Filter Design
+
+Designed a 4-tap FIR Low Pass Filter in Verilog.
+
+## Filter Specifications
+- Type: FIR Low Pass Filter
+- Taps: 4
+- Coefficients: [1, 2, 2, 1]
+- Input Width: 8-bit
+- Output Width: 19-bit
+
+## Simulation Results
+| Time | Input | Output |
+|------|-------|--------|
+| 15   | 10    | 10     |
+| 25   | 20    | 40     |
+| 35   | 30    | 90     |
+| 45   | 40    | 150    |
+| 55   | 50    | 210    |
+| 75   | 0     | 140    |
+| 85   | 0     | 50     |
+
+## Tool Used
+EDA Playground - Riviera PRO
+
+## Intern
+Name: Harshavardhan
+Intern ID: CTIS8866
